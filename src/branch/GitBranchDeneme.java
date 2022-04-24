a06994eeaefebe19a238083975105671530eeac6
Mehmet Akif Yuksel <67182475+MemetAkif@users.noreply.github.com>
2022-04-24 13:04:55 +0200

    Update GitBranchDeneme.java

--- src/branch/GitBranchDeneme.java
+++ src/branch/GitBranchDeneme.java
@@ -5,6 +5,7 @@ public class GitBranchDeneme {
 	public static void main(String[] args) {
 		System.out.println("Feature banch 1. satir");
 		System.out.println("Feature banch 2. satir");
+		System.out.println("Main banch 3. satir githubdan eklenen 1");
 
 		
 
